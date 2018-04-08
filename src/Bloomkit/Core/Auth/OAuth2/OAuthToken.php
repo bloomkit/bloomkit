d6d10d2f758b4e3fbc1b5e29ef751ff91a29baff
karlbach <bloomkit@probelix.de>
2018-04-08 08:48:20 +0200

    Added OAuthToken and test

---
+++ src/Bloomkit/Core/Auth/OAuth2/OAuthToken.php
@@ -0,0 +1,175 @@
+<?php
+
+namespace Bloomkit\Core\Auth\OAuth2;
+
+/**
+ * Representation of a generic OAuth token (authCode, authToken & refreshToken).
+ */
+class OAuthToken
+{
+    /**
+     * @var string
+     */
+    protected $clientId;
+
+    /**
+     * @var string
+     */
+    protected $token;
+
+    /**
+     * @var int
+     */
+    protected $expiresAt;
+
+    /**
+     * @var string
+     */
+    protected $scope;
+
+    /**
+     * @var string
+     */
+    protected $userId;
+
+    /**
+     * Constructor.
+     *
+     * @param string $clientId  The id of the corresponding client
+     * @param string $userId    The id of the user this token is for
+     * @param string $token     The token code
+     * @param int    $expiresAt Timestamp when the token expired
+     * @param string $scope     The scope of the token
+     */
+    public function __construct($clientId, $userId, $token, $expiresAt = null, $scope = null)
+    {
+        $this->userId = $userId;
+        $this->setClientId($clientId);
+        $this->setToken($token);
+        $this->setExpiresAt($expiresAt);
+        $this->setScope($scope);
+    }
+
+    /**
+     * Returns the client id.
+     *
+     * @return string The id of the corresponding client
+     */
+    public function getClientId()
+    {
+        return $this->clientId;
+    }
+
+    /**
+     * Returns the duration in seconds when the token expires.
+     *
+     * @return int The time in seconds when this token expires
+     */
+    public function getExpiresIn()
+    {
+        if ($this->expiresAt) {
+            $expires = $this->expiresAt - time();
+            if ($expires < 0) {
+                $expires = 0;
+            }
+
+            return $expires;
+        } else {
+            return PHP_INT_MAX;
+        }
+    }
+
+    /**
+     * Returns the scope of the token.
+     *
+     * @return string The scope of the token
+     */
+    public function getScope()
+    {
+        return $this->scope;
+    }
+
+    /**
+     * Returns the token code.
+     *
+     * @return string The token code
+     */
+    public function getToken()
+    {
+        return $this->token;
+    }
+
+    /**
+     * Returns the associated user-id.
+     *
+     * @return string The id of the user
+     */
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+
+    /**
+     * Check if token is expired.
+     *
+     * @return bool true if expired, false if not
+     */
+    public function hasExpired()
+    {
+        if ($this->expiresAt <= 0) {
+            return false;
+        }
+
+        return time() > $this->expiresAt;
+    }
+
+    /**
+     * Set the client id.
+     *
+     * @param string $id The id of the corresponding client
+     */
+    public function setClientId($id)
+    {
+        $this->clientId = $id;
+    }
+
+    /**
+     * Set the expiration timestamp.
+     *
+     * @param int $timestamp The timestamp when this token expires
+     */
+    public function setExpiresAt($timestamp)
+    {
+        $this->expiresAt = $timestamp;
+    }
+
+    /**
+     * Set the scope.
+     *
+     * @param string $scope The scope of the token
+     */
+    public function setScope($scope)
+    {
+        $this->scope = $scope;
+    }
+
+    /**
+     * Set the token.
+     *
+     * @param string $token The token code
+     */
+    public function setToken($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Set the user id.
+     *
+     * @param string $id The id of the user
+     */
+    public function setUserId($id)
+    {
+        $this->userId = $id;
+    }
+}
---
+++ src/Bloomkit/Core/Auth/OAuth2/Tests/OAuthTokenTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Bloomkit\Core\Auth\OAuth2\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Bloomkit\Core\Auth\OAuth2\OAuthToken;
+
+class OAuthTokenTest extends TestCase
+{
+    public function testConstructor()
+    {
+        $token = new OAuthToken('clientid', 'userid', 'token', time() + 10, 'scope');
+        $this->assertEquals($token->getClientId(), 'clientid');
+        $this->assertEquals($token->getUserId(), 'userid');
+        $this->assertEquals($token->getToken(), 'token');
+        $this->assertEquals($token->getExpiresIn(), 10);
+        $this->assertEquals($token->getScope(), 'scope');
+    }
+
+    public function testSetToken()
+    {
+        $token = new OAuthToken('', '', '');
+        $token->setToken('token');
+        $this->assertEquals($token->getToken(), 'token');
+    }
+
+    public function testSetClientId()
+    {
+        $token = new OAuthToken('', '', '');
+        $token->setClientId('clientid');
+        $this->assertEquals($token->getClientId(), 'clientid');
+    }
+
+    public function testSetUserId()
+    {
+        $token = new OAuthToken('', '', '');
+        $token->setUserId('userid');
+        $this->assertEquals($token->getUserId(), 'userid');
+    }
+
+    public function testSetScope()
+    {
+        $token = new OAuthToken('', '', '');
+        $token->setScope('scope');
+        $this->assertEquals($token->getScope(), 'scope');
+    }
+
+    public function testSetExpires()
+    {
+        $token = new OAuthToken('', '', '');
+        $token->setExpiresAt(time() + 10);
+        $this->assertEquals($token->getExpiresIn(), 10);
+        $this->assertEquals($token->hasExpired(), false);
+        $token->setExpiresAt(time() - 10);
+        $this->assertEquals($token->getExpiresIn(), 0);
+        $this->assertEquals($token->hasExpired(), true);
+        $token->setExpiresAt(null);
+        $this->assertEquals($token->getExpiresIn(), PHP_INT_MAX);
+        $this->assertEquals($token->hasExpired(), false);
+    }
+}
