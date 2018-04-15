8c204c92cc66005ba809526ffbdb008c1292c5a0
karlbach <bloomkit@probelix.de>
2018-04-15 19:25:54 +0200

    Added RestApplication Added RestExceptionListener

---
+++ src/Bloomkit/Core/Rest/ExceptionListener.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Bloomkit\Core\Rest;
+
+use Psr\Log\LoggerInterface;
+use Bloomkit\Core\Security\Exceptions\CredentialsMissingException;
+use Bloomkit\Core\Security\Exceptions\AccessDeniedException;
+use Bloomkit\Core\Security\Exceptions\AuthFailedException;
+use Bloomkit\Core\Http\Exceptions\HttpNotFoundException;
+use Bloomkit\Core\Security\Exceptions\BadCredentialsException;
+use Bloomkit\Core\Http\HttpExceptionEvent;
+
+/**
+ * ExceptionListener for REST applications.
+ *
+ * Generates error-responses and logs
+ */
+class ExceptionListener
+{
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * Constructor.
+     *
+     * @param LoggerInterface $logger Logger to log messages to
+     */
+    public function __construct(LoggerInterface $logger = null)
+    {
+        $this->logger = $logger;
+    }
+
+    /**
+     * EventHandler for HttpExceptions.
+     *
+     * @param HttpExceptionEvent $event The exception-event to handle
+     */
+    public function onException(HttpExceptionEvent $event)
+    {
+        $exception = $event->getException();
+        $request = $event->getRequest();
+
+        if ($exception instanceof BadCredentialsException) {
+            if (isset($this->logger)) {
+                $this->logger->warning('BadCredentials Error:'.$exception->getMessage());
+            }
+            $response = RestResponse::createFault(403, 'No credentials found');
+        } elseif ($exception instanceof CredentialsMissingException) {
+            if (isset($this->logger)) {
+                $this->logger->warning('BadCredentials Error:'.$exception->getMessage());
+            }
+            $response = RestResponse::createFault(401, 'No credentials found');
+        } elseif ($exception instanceof HttpNotFoundException) {
+            if (isset($this->logger)) {
+                $this->logger->warning('Not found:'.$exception->getMessage());
+            }
+            $response = RestResponse::createFault(404, $exception->getMessage());
+        } elseif ($exception instanceof AuthFailedException) {
+            if (isset($this->logger)) {
+                $this->logger->warning('Authentication failed:'.$exception->getMessage());
+            }
+            $response = RestResponse::createFault(401, $exception->getMessage(), $exception->getCode());
+        } elseif ($exception instanceof AccessDeniedException) {
+            if (isset($this->logger)) {
+                $this->logger->warning('Access denied:'.$exception->getMessage());
+            }
+            $response = RestResponse::createFault(403, $exception->getMessage());
+        }
+
+        if (isset($response)) {
+            $event->setResponse($response);
+        }
+    }
+}
---
+++ src/Bloomkit/Core/Rest/RestApplication.php
@@ -0,0 +1,260 @@
+<?php
+
+namespace Bloomkit\Core\Rest;
+
+use Bloomkit\Core\Application\Application;
+use Bloomkit\Core\Application\ModuleInterface;
+use Bloomkit\Core\Security\Exceptions\AuthConfigException;
+use Bloomkit\Core\Http\HttpEvent;
+use Bloomkit\Core\Http\HttpEvents;
+use Bloomkit\Core\Http\HttpExceptionEvent;
+use Bloomkit\Core\Http\Exceptions\HttpNotFoundException;
+use Bloomkit\Core\Routing\Exceptions\RessourceNotFoundException;
+
+class RestApplication extends Application
+{
+    /**
+     * @var array
+     */
+    private $supportedVersions = ['v1'];
+
+    /**
+     * Constuctor.
+     *
+     * {@inheritdoc}
+     */
+    public function __construct($appName, $appVersion, $basePath = null, array $config = [])
+    {
+        parent::__construct($appName, $appVersion, $basePath, $config);
+
+        $this->registerFactory('routes', 'Bloomkit\Core\Routing\RouteCollection', true);
+        $this->registerFactory('exception_handler', 'Bloomkit\Core\Rest\ExceptionListener', true);
+        $this->registerFactory('url_matcher', 'Bloomkit\Core\Routing\UrlMatcher', true);
+
+        $aliases = [
+            'routes' => ['Bloomkit\Core\Routing\RouteCollection'],
+        ];
+
+        foreach ($aliases as $key => $aliases) {
+            foreach ($aliases as $alias) {
+                $this->setAlias($key, $alias);
+            }
+        }
+
+        $this->getEventManager()->addListener(HttpEvents::EXCEPTION, [$this['exception_handler'], 'onException']);
+    }
+
+    /**
+     * Processing the request.
+     *
+     * @param RestRequest $request The request to process
+     *
+     * @return RestResponse The response to the request
+     */
+    private function process(RestRequest $request)
+    {
+        // Check if version is supported
+        $apiVersion = $request->getApiVersion();
+        if ('' == $apiVersion) {
+            return RestResponse::createFault(400, 'Invalid request: The api version is missing', 31030);
+        }
+        if (false === array_search($apiVersion, $this->supportedVersions)) {
+            return RestResponse::createFault(400, 'The requested api version is not supported by this server.', 31030);
+        }
+
+        $event = new HttpEvent($request);
+        $this['eventManager']->triggerEvent(HttpEvents::REQUEST, $event);
+
+        if ($event->hasResponse()) {
+            $this['eventManager']->triggerEvent(HttpEvents::RESPONSE, $event);
+            $this['eventManager']->triggerEvent(HttpEvents::FINISH_REQUEST, $event);
+
+            return $event->getResponse();
+        }
+
+        try {
+            $tracer = $this->getTracer();
+
+            $tracer->start('App::findRoute');
+            $matcher = $this->getUrlMatcher();
+            $parameters = $matcher->match($request->getPathUrl(), $request->getHttpMethod());
+            $tracer->stop('App::findRoute');
+
+            // Authentication
+            if (isset($parameters['_auth'])) {
+                $auth = $parameters['_auth'];
+
+                if ((isset($auth['authEntryPoint'])) && (class_exists($auth['authEntryPoint']))) {
+                    $this->get('firewall')->setAuthEntryPoint(new $auth['authEntryPoint']());
+                }
+
+                if (false == isset($auth['authenticator'])) {
+                    throw new AuthConfigException(sprintf('"authenticator" parameter is missing in route-config for "%s"', $request->getPathUrl()));
+                }
+                if (false == isset($auth['userProvider'])) {
+                    throw new AuthConfigException(sprintf('"userProvider" parameter is missing in route-config for "%s"', $request->getPathUrl()));
+                }
+
+                if (!class_exists($auth['authenticator'])) {
+                    throw new \InvalidArgumentException(sprintf('Class "%s" does not exist.', $auth['authenticator']));
+                }
+                if (!class_exists($auth['userProvider'])) {
+                    throw new \InvalidArgumentException(sprintf('Class "%s" does not exist.', $auth['userProvider']));
+                }
+                if (is_subclass_of($auth['userProvider'], 'Bloomkit\Core\Security\User\EntityUserProvider')) {
+                    $userProvider = new $auth['userProvider']($this->entityManager);
+                } else {
+                    $userProvider = new $auth['userProvider']();
+                }
+
+                $authenticator = new $auth['authenticator']($userProvider);
+                $token = $authenticator->createToken($request);
+
+                if (false == $authenticator->supportsToken($token)) {
+                    throw new \Exception('Token is not supported');
+                }
+
+                $token = $authenticator->authenticateToken($token, $userProvider);
+                $this->getSecurityContext()->setToken($token);
+            }
+
+            $request->attributes->addItems($parameters);
+            $controllerName = $parameters['_controller'];
+
+            $this['eventManager']->triggerEvent(HttpEvents::CONTROLLER, $event);
+
+            if (false === strpos($controllerName, '::')) {
+                throw new \InvalidArgumentException(sprintf('Unable to find controller "%s".', $controllerName));
+            }
+
+            $controllerInfo = list($class, $method) = explode('::', $controllerName, 2);
+
+            if (!class_exists($class)) {
+                throw new \InvalidArgumentException(sprintf('Class "%s" does not exist.', $class));
+            }
+
+            if (is_array($controllerInfo)) {
+                $r = new \ReflectionMethod($controllerInfo[0], $controllerInfo[1]);
+            }
+
+            $params = $r->getParameters();
+
+            $attributes = $request->attributes->getItems();
+            $arguments = [];
+
+            foreach ($params as $param) {
+                if (array_key_exists($param->name, $attributes)) {
+                    $arguments[] = $attributes[$param->name];
+                } elseif ($param->getClass() && $param->getClass()->isInstance($request)) {
+                    $arguments[] = $request;
+                } elseif ($param->isDefaultValueAvailable()) {
+                    $arguments[] = $param->getDefaultValue();
+                } else {
+                    if (is_array($controller)) {
+                        $repr = sprintf('%s::%s()', $controller[0], $controller[1]);
+                    } elseif (is_object($controller)) {
+                        $repr = get_class($controller);
+                    } else {
+                        $repr = $controller;
+                    }
+                    throw new \RuntimeException(sprintf('Controller "%s" requires that you provide a value for the "$%s" argument (because there is no default value or because there is a non optional argument after this one).', $repr, $param->name));
+                }
+            }
+
+            $controller = new $class($this);
+            $controller->setRequest($request);
+
+            $tracer->start('App::CallController');
+            $response = call_user_func_array([$controller, $method], $arguments);
+            $tracer->stop('App::CallController');
+
+            $this['eventManager']->triggerEvent(HttpEvents::VIEW, $event);
+            $event->setResponse($response);
+            $this['eventManager']->triggerEvent(HttpEvents::RESPONSE, $event);
+            $this['eventManager']->triggerEvent(HttpEvents::FINISH_REQUEST, $event);
+
+            return $response;
+        } catch (RessourceNotFoundException $e) {
+            $message = sprintf('No route found for "%s %s"', $request->getHttpMethod(), $request->getFullPathUrl());
+            throw new HttpNotFoundException($message);
+        } catch (MethodNotAllowedException $e) {
+            $message = sprintf('No route found for "%s %s": Method Not Allowed (Allow: %s)', $request->getMethod(), $request->getFullPathUrl(), implode(', ', $e->getAllowedMethods()));
+            throw new MethodNotAllowedHttpException($e->getAllowedMethods(), $message, $e);
+        }
+    }
+
+    /**
+     * Handles an exception by trying to convert it to a Response.
+     *
+     * @param \Exception  $e
+     * @param RestRequest $request
+     *
+     * @return RestResponse
+     *
+     * @throws \Exception
+     */
+    private function handleException(\Exception $e, RestRequest $request)
+    {
+        $event = new HttpExceptionEvent($request, $e);
+        $this['eventManager']->triggerEvent(HttpEvents::EXCEPTION, $event);
+
+        // a listener might have replaced the exception
+        $e = $event->getException();
+
+        if (!$event->hasResponse()) {
+            throw $e;
+        }
+
+        return $event->getResponse();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function registerModule(ModuleInterface $module)
+    {
+        parent::registerModule($module);
+        $routes = $module->getRoutes();
+
+        if ((is_array($routes)) && ($routes->getCount() > 0)) {
+            $this['routes']->addCollection($routes);
+        }
+    }
+
+    /**
+     * Start the application.
+     *
+     * @param RestRequest $request The request to process.
+     */
+    public function run(RestRequest $request = null)
+    {
+        try {
+            if (is_null($request)) {
+                $request = RestRequest::processRequest();
+            }
+            $response = $this->process($request);
+        } catch (\Exception $e) {
+            $response = $this->handleException($e, $request);
+        }
+
+        $event = new HttpEvent($request);
+        $event->setResponse($response);
+        $this['eventManager']->triggerEvent(HttpEvents::RESPONSE, $event);
+        $response = $event->getResponse();
+        $response->send();
+        $this['eventManager']->triggerEvent(HttpEvents::TERMINATE, $event);
+        exit();
+    }
+
+    /**
+     * Set the supported API versions.
+     *
+     * The API version must be used in every request: https://example.com/api/v1/
+     *
+     * @param array $versions The API versions to support
+     */
+    public function setSupportedApiVersions(array $versions)
+    {
+        $this->supportedVersions = $versions;
+    }
+}
