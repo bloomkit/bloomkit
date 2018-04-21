3fb8cd55849ca555dd5f1425b1567e03a8808269
karlbach <bloomkit@probelix.de>
2018-04-21 16:30:54 +0200

    Reorganization of the UrlMatcher

--- src/Bloomkit/Core/Routing/UrlMatcher.php
+++ src/Bloomkit/Core/Routing/UrlMatcher.php
@@ -1,37 +1,95 @@
 <?php
+
 namespace Bloomkit\Core\Routing;
 
-use Bloomkit\Core\Routing\Exceptions\RessourceNotFoundException;
-
+/**
+ * Class for finding matching routes for an url.
+ */
 class UrlMatcher
-{	
-	protected $routes;
-	
-	protected $allow = [];
+{
+    /**
+     * @var RouteCollection
+     */
+    protected $routes;
 
-	public function __construct(RouteCollection $routes)
-	{
-		$this->routes  = $routes;
-	}	
-	
-	public function match($pathinfo, $method = null)
-	{
-	    $this->allow = [];
-	
-		if ($ret = $this->matchCollection(rawurldecode($pathinfo), $this->routes, $method))
-			return $ret;
-	
-		if (count($this->allow) > 0)
-			throw new MethodNotAllowedException(array_unique(array_map('strtoupper', $this->allow)));
-		else
-			throw new RessourceNotFoundException();
-	}
-	
-	protected function matchCollection($pathinfo, RouteCollection $routes, $method = null)
-	{
-	    foreach ($routes->getRoutes() as $name => $route) {
-
-	    }
+    /**
+     * Constructor.
+     *
+     * @param RouteCollection $routes The routes to match
+     */
+    public function __construct(RouteCollection $routes)
+    {
+        $this->routes = $routes;
     }
-	
+
+    /**
+     * Find a matching route for a path (+method +host).
+     *
+     * @param string      $path   The url-path to find a matching route for
+     * @param string|null $method An optional HTTP-method requirement
+     * @param string|null $host   An optional host requirement
+     */
+    public function match($path, $method = null, $host = null)
+    {
+        if ($match = $this->matchCollection(rawurldecode($path), $this->routes, $method, $host)) {
+            return $match;
+        }
+
+        throw new RessourceNotFoundException();
+    }
+
+    /**
+     * Search the route collection for the requested path (+method + host).
+     *
+     * @param string          $path   The url-path to find a matching route for
+     * @param RouteCollection $routes The routes to check for a match
+     * @param string|null     $method An optional HTTP-method requirement
+     * @param string|null     $host   An optional host requirement
+     */
+    protected function matchCollection($path, RouteCollection $routes, $method = null, $host = null)
+    {
+        foreach ($routes->getRoutes() as $name => $route) {
+            $compiledRoute = $route->compile();
+
+            $staticPrefix = $compiledRoute->getStaticPrefix();
+
+            //quickcheck if route may match
+            if (('' !== $staticPrefix) && (0 !== strpos($path, $staticPrefix))) {
+                continue;
+            }
+
+            //if it does, check the regex
+            if (!preg_match($compiledRoute->getRegex(), $path, $matches)) {
+                continue;
+            }
+
+            //check if host matches
+            $hostMatches = [];
+            if (isset($host)) {
+                $hostRegex = $compiledRoute->getHostRegex();
+                if ($hostRegex && !preg_match($hostRegex, $host, $hostMatches)) {
+                    continue;
+                }
+            }
+
+            //check if method matches
+            if (isset($method)) {
+                $supportedMethods = $route->getMethods();
+                // HEAD and GET are equivalent as per RFC
+                if ('HEAD' == $method) {
+                    $method = 'GET';
+                }
+
+                if (false == in_array($method, $supportedMethods)) {
+                    continue;
+                }
+            }
+
+            //return the defaults of the matching route
+            $defaults = $route->getDefaults();
+            $defaults['_route'] = $name;
+
+            return $defaults;
+        }
+    }
 }
