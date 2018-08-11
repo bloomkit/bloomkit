b72aa70f28968af7bc59fcdb569eef829e5e50f4
karlbach <bloomkit@probelix.de>
2018-08-11 11:50:52 +0200

    Added missing UserEvent

---
+++ src/Bloomkit/Core/Security/User/UserEvent.php
@@ -0,0 +1,37 @@
+<?php
+namespace Bloomkit\Core\Security\Users;
+
+use Bloomkit\Core\EventManager\Event;
+
+/**
+ * Event concerning User actions.
+ */
+class UserEvent extends Event
+{
+    /**
+     * @var User
+     */
+    private $user;
+
+    /**
+     * Constructor
+     *
+     * @param User $user The User object this event is about
+     */
+    public function __construct(User $user)
+    {
+        parent::__construct();
+        $this->user = $user;
+    }
+    
+    /**
+     * Return the User object
+     * 
+     * @return User The User object of this event
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+}
