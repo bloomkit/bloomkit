f65f454e412935af135285f16866aefec4aa77e9
Karl Bach <bloomkit@probelix.de>
2019-07-03 12:40:01 +0200

    Added ConsoleInit constant

--- src/Bloomkit/Core/Console/Events/ConsoleEvents.php
+++ src/Bloomkit/Core/Console/Events/ConsoleEvents.php
@@ -8,6 +8,7 @@ class ConsoleEvents extends Event
 {
     const REGISTEROPTIONS = 'console.registeroptions';
     const CONSOLERUN = 'console.run';
+    const CONSOLEINIT = 'console.init';
 
     public function __construct()
     {
